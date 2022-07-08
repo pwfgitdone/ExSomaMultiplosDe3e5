41ecb0b4b81464e8ba16c29c0f67ae242dd88587
pwfgitdone <ddrdan7@hotmail.com>
2022-07-08 14:42:49 -0300

    Soma de todos os números naturais de 0 até o número informado, múltiplos de 3 ou 5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número natural: ");
+            int limite = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 0; i <= limite; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine("Soma de todos os números naturais de 0 até o número informado, múltiplos de 3 ou 5: " + soma);
+        }
+    }
+}
